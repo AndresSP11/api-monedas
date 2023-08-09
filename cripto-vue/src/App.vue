ee5c1a9b54dd1540b4e176fafb796eca2d12e33f
Andres <andres.salazar.p@uni.pe>
2023-08-09 12:43:51 -0500

    Trabajao añadido

---
+++ cripto-vue/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import {ref} from 'vue'
+  const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+      { codigo: 'PEN', texto: 'Sol Peruano'}
+  ])
+</script>
+
+<template>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+  </div>
+  <div class="contenido">
+    <form class="formulario">
+      <div class="campo">
+        <label for="moneda">Moneda:</label>
+        <select id="moneda">
+          <option value="">--Seleccione--</option>
+          <option  v-for="moneda in monedas"
+          :value="moneda.codigo"
+          >{{ moneda.texto }}</option>
+        </select>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
